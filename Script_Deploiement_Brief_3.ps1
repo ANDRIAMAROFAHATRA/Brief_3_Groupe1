0d79b056b09b0afa12cd88ec9e3db5acd0d4e4b4
stephane_andria <stephane.andriamarofahatra@gmail.com>
2022-07-22 12:14:09 +0200

    mise à jour du script création réseau sous réseau

--- Script_Deploiement_Brief_3.ps1
+++ Script_Deploiement_Brief_3.ps1
@@ -4,3 +4,22 @@ while ($(az group exists -n GiteaFirst) -eq $false) {
     Write-Host "Ressource Groupe Create"
 }
 Write-Host "Etape2"
+
+az network vnet create `
+    -g GiteaFirst `
+    -n GiteaVnet `
+    --address-prefix 10.0.1.0/24 `
+   
+az network vnet subnet create `
+    -g GiteaFirst `
+    --vnet-name GiteaVnet `
+    --name SubnetBastion `
+    --address-prefixes 10.0.1.64/26 `
+
+az network vnet subnet create `
+    -g GiteaFirst `
+    --vnet-name GiteaVnet `
+    --name GiteaSubnet `
+    --address-prefixes 10.0.1.0/29 `
+
+az network vnet subnet list -g GiteaFirst --vnet-name GiteaVnet
