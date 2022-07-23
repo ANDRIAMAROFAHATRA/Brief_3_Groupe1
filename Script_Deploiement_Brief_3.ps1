231effefd56f10edd14acc48fe72f8b629521cc4
simplon-gabrielsarais <sarais.gabriel@gmail.com>
2022-07-23 09:24:28 +0200

    ajout de la détéction d'erreur

--- Script_Deploiement_Brief_3.ps1
+++ Script_Deploiement_Brief_3.ps1
@@ -1,29 +1,44 @@
 try {
-    Start-Process az -ErrorVariable errcmd
-    az group create -l francecentral -n GiteaFirst 
+
+az group create -l francecentral -n GiteaFirst
+    if ($? -eq $false) {
+        $erreur = "la création du groupe de ressource GiteaFirst a échoué"
+        throw ''
+    }
 
 az network vnet create `
     -g GiteaFirst `
     -n GiteaVnet `
-    --address-prefix 10.0.1.0/24 `
+    --address-prefix 10.0.1.0/24
+    if ($? -eq $false) {
+        $erreur = "la création du Vnet GiteaVnet a échoué"
+        throw ''
+    }
 
 az network vnet subnet create `
     -g GiteaFirst `
     --vnet-name GiteaVnet `
     --name SubnetBastion `
-    --address-prefixes 10.0.164/26 `
+    --address-prefixes 10.0.1.64/26
+    if ($? -eq $false) {
+        $erreur = "la création du Subnet SubnetBastion a échoué"
+        throw ''
+    }
 
 az network vnet subnet create `
     -g GiteaFirst `
     --vnet-name GiteaVnet `
     --name GiteaSubnet `
-    --address-prefixes 10.az0.1.0/29 `
-
-az network vnet subnet list -g GiteaFirst --vnet-name GiteaVnet
+    --address-prefixes 10.az0.1.0/29
+    if ($? -eq $false) {
+        $erreur = "la création du Subnet GiteaSubnet a échoué"
+        throw ''
+    }
 
 }
 
 catch {
-    write-host $Error
+    Write-Host $ErrorView
+    write-host $Erreur
     az group delete -n GiteaFirst -y
 }
