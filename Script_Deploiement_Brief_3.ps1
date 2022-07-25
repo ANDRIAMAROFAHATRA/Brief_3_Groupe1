9bacc4c5d4686fa2354b9312548c0eba63bc97f9
simplon-gabrielsarais <sarais.gabriel@gmail.com>
2022-07-25 10:40:08 +0200

    ajout de l'affichage des erreurs

--- Script_Deploiement_Brief_3.ps1
+++ Script_Deploiement_Brief_3.ps1
@@ -1,9 +1,9 @@
+$error.Clear()
 try {
 
 az group create -l francecentral -n GiteaFirst
     if ($? -eq $false) {
-        $erreur = "la création du groupe de ressource GiteaFirst a échoué"
-        throw ''
+        throw 'la création du groupe de ressource GiteaFirst a échoué'
     }
 
 az network vnet create `
@@ -11,8 +11,7 @@ az network vnet create `
     -n GiteaVnet `
     --address-prefix 10.0.1.0/24
     if ($? -eq $false) {
-        $erreur = "la création du Vnet GiteaVnet a échoué"
-        throw ''
+        throw 'la création du Vnet GiteaVnet a échoué'
     }
 
 az network vnet subnet create `
@@ -21,24 +20,22 @@ az network vnet subnet create `
     --name SubnetBastion `
     --address-prefixes 10.0.1.64/26
     if ($? -eq $false) {
-        $erreur = "la création du Subnet SubnetBastion a échoué"
-        throw ''
+        throw 'la création du Subnet SubnetBastion a échoué'
     }
 
 az network vnet subnet create `
     -g GiteaFirst `
     --vnet-name GiteaVnet `
     --name GiteaSubnet `
-    --address-prefixes 10.az0.1.0/29
+    --address-prefixes 10.0.10/29 
     if ($? -eq $false) {
-        $erreur = "la création du Subnet GiteaSubnet a échoué"
-        throw ''
+        throw 'la création du Subnet GiteaSubnet a échoué'
     }
 
 }
 
 catch {
-    Write-Host $ErrorView
-    write-host $Erreur
+    write-host $error
+    write-host "les ressource Azure crées vont être supprimées:"
     az group delete -n GiteaFirst -y
 }
