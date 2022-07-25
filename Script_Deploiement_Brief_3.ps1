2b0834bcfdfe9b2f24ea1f81b405c3806f9942d9
stephane_andria <stephane.andriamarofahatra@gmail.com>
2022-07-25 12:13:45 +0200

    rajout de l'IP public + bastion

--- Script_Deploiement_Brief_3.ps1
+++ Script_Deploiement_Brief_3.ps1
@@ -18,7 +18,7 @@ az network vnet create `
 az network vnet subnet create `
     -g GiteaFirst `
     --vnet-name GiteaVnet `
-    --name SubnetBastion `
+    --name AzureBastionSubnet `
     --address-prefixes 10.0.1.64/26
     if ($? -eq $false) {
         $erreur = "la création du Subnet SubnetBastion a échoué"
@@ -35,10 +35,45 @@ az network vnet subnet create `
         throw ''
     }
 
+az network public-ip create `
+    -g GiteaFirst -n MyFirstPublicIpBastion --sku Standard -z 1
+     if ($? -eq $false) {
+        $erreur = "la création de l'IP public Bastion a échoué"
+        throw ''
+    }
+
+    az network bastion create --only-show-errors -l francecentral `
+     -n Bastion `
+	--public-ip-address MyFirstPublicIpBastion `
+	-g GiteaFirst `
+    --vnet-name GiteaVnet
+     if ($? -eq $false) {
+        $erreur = "la création du service Bastion a échoué"
+        throw ''
+    }
+    az mysql server create -l francecentral `
+    -g GiteaFirst `
+    -n GiteaSQLsvr `
+    -u Gitea `
+     -p $Env:passwdSQL `
+     --sku-name B_Gen5_1 `
+     --ssl-enforcement Enabled `
+     --minimal-tls-version TLS1_0 `
+     --public-network-access Disabled `
+	--backup-retention 14 `
+    --geo-redundant-backup Enabled `
+    --storage-size 51200 `
+    --tags "key=value" `
+    --version 5.7
+    if ($? -eq $false) {
+        $erreur = "la création du serveur MYSQL a échoué"
+        throw ''
+    }
 }
 
 catch {
+    Write-Host "In CATCH"
     Write-Host $ErrorView
     write-host $Erreur
-    az group delete -n GiteaFirst -y
+    #az group delete -n GiteaFirst -y
 }
