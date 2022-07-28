60429616ced8a617ad7ed040ed9324185140227a
Simplon-CeliaOuedraogo <108000888+Simplon-CeliaOuedraogo@users.noreply.github.com>
2022-07-28 11:31:55 +0200

    Update Script_Connexion_VM.ps1

--- Script_Connexion_VM.ps1
+++ Script_Connexion_VM.ps1
@@ -1,8 +1,8 @@
 ﻿#----------INITIALISATION DES VARIABLES SELON POSTE LOCAL -------------------------
 
 
-$Rsa_Path_Local = 'C:\Users\utilisateur\.ssh\id_rsa'
-$RessourceGroupName = 'Gitea_First'
+$Rsa_Path_Local = '<chemin cle priv>'
+$RessourceGroupName = 'GiteaFirst'
 $NameBastion = 'Bastion'
 
 #-------------PARSE JSON DE L' ID DE LA VM + CONNEXION ----------------------------
