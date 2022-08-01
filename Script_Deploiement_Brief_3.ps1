db222d9eefeade21af60fc16b86033c7fa86b0fd
simplon-gabrielsarais <sarais.gabriel@gmail.com>
2022-08-01 16:19:40 +0200

    ouais trop bien

--- Script_Deploiement_Brief_3.ps1
+++ Script_Deploiement_Brief_3.ps1
@@ -201,7 +201,7 @@ if ($step -lt 9){
 }
 
 #----------------------CREATION DE LA VM GITEA----------------------------
-(Get-Content .\cloud-init.txt) -replace 'PASSWD   = MOTDEPASSE', 'PASSWD   = $Env:passwdSQL' | Out-File .\cloud-init.txt
+(Get-Content .\cloud-init.txt) -replace 'PASSWD   = MOTDEPASSE', "PASSWD   = $Env:passwdSQL" | Out-File .\cloud-init.txt
 
 if ($step -lt 10) {
     $sortie = az vm create -n $NameVM -g $RessourceGroupName `
@@ -222,7 +222,7 @@ if ($step -lt 10) {
             Write-Host "La VM a été créé avec succès" -ForegroundColor Yellow
         }
     }
-    (Get-Content .\cloud-init.txt) -replace 'PASSWD   = $Env:passwdSQL', 'PASSWD   = MOTDEPASSE' | Out-File .\cloud-init.tx
+    (Get-Content .\cloud-init.txt) -replace "PASSWD   = $Env:passwdSQL", 'PASSWD   = MOTDEPASSE' | Out-File .\cloud-init.txt
 #--------------------------firewall_MySQL---------------------------------------------
     $ipserver = az vm show -d --resource-group $RessourceGroupName -n $NameVM --query publicIps -o tsv
 
